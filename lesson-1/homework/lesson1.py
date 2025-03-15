b5c772951b9480ad91adcb1468f3e6e494212f38
Abdumalikov3395 <abdumalikov3395@gmail.com>
2025-03-15 19:11:32 +0500

    lesson1.py

--- lesson-1/homework/lesson1.py
+++ lesson-1/homework/lesson1.py
@@ -1,1 +1,32 @@
+#Task 1
+
+print("Write a length of square")
+a=int(input())
+print(f"Perimetr={a*4}")
+print(f"Area={a*a}")
+
+#Task 2
+
+print("write diameter of circle")
+a=int(input())
+print(f"Lenght of circle={round(a*3.14,2)}")
+
+#Task 3
+
+print("Write a")
+a=int(input())
+print("Write b")
+b=int(input())
+print(f"Average of {a} and {b} ={round((a+b)/2,2)}")
+
+#Task 4
+
+print("Write a")
+a=int(input())
+print("Write b")
+b=int(input())
+print(f"Sum of {a} and {b} = {(a+b)}")
+print(f"Product of {a} and {b} = {(a*b)}")
+print(f"Square of {a} = {a*a}")
+print(f"Square of {b} = {b*b}")
 
